7e9c72ea3eada6f26b271e56bdf623db86803516
Johnny Hildebrand <74218092+JohnnyHildebrand@users.noreply.github.com>
2022-08-26 10:53:48 -0500

    add badge from bootstrap changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function NavBar(props) {
+
+
+function NavBar({totalCounters}) {
   return (
-    <nav className="navbar-light bg-light">
+    <nav className="navbar navbar-light bg-light">
       <div className="navbar-brand">
-        <i className="fa fa-shoppping-cart fa-lg m-2" />
+        <i className="fa fa-shopping-cart fa-lg m-2"/>
         <span
-          className="badge badge-pill badge-info m-2"
+          className="badge rounded-pill text-bg-primary"
           style={{ width: 50 }}>
-          </span>
+          {totalCounters}
+          </span> 
         Items
       </div>
     </nav>
