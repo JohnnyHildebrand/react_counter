5ec56fe6e7a1587065f3ffa4c2cb92fb48e764a0
Johnny Hildebrand <74218092+JohnnyHildebrand@users.noreply.github.com>
2022-08-29 23:15:13 -0500

    Final Polish

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
     counters: [
         { id: 1, value: 0 },
         { id: 2, value: 0 },
-        { id: 3, value: 0 }
+        { id: 3, value: 0 },
+        { id: 4, value: 0 },
+        { id: 5, value: 0 }
+
     ]
 };
 
